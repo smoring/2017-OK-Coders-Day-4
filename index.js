09e0539d30d61f2e04d14f0a4adaccb74899acdc
Susan Moring <susanmoring@susans-air.hsc.net.ou.edu>
2017-04-15 09:59:26 -0500

    Other verb examples

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ var restify = require('restify');
 var server = restify.createServer();
 var port = 8088;
 
-server.get('/', function(req, res, next){
-	res.send("Hello World");
-	next();
-});
+function send(req, res, next){
+	res.send("Testing " + req.params.test);
+	return next;
+}
 
-server.get('/hello/:name', function(req, res, next){
-	res.send('Hello ' + req.params.name);
-	next();
-});
+server.get('/', send);
+server.get('/hello/:test', send);
+server.put('/hello/:test', send);
+server.del('/hello/:test', function(req, res, next){
+	res.send(204);
+	return next();
+})
 
 server.listen(port, function(){
 	console.log('%s listening at %s', server.name, port);
